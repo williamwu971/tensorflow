299d16b47b498d470daa7328d95e88c15aa5ba1e
xiaoxiang.wu <xiaoxiang.wu@sydney.edu.au>
2023-11-06 14:43:22 +1100

    auto

--- build.bash
+++ build.bash
@@ -8,35 +8,37 @@ fi
 
 rm -f /tmp/tensorflow_pkg/*
 
-/usr/local/bin/bazel build --config=dbg \
-    --per_file_copt=+tensorflow/compiler/xla/pjrt/transpose_kernels.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/gl_delegate.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/delegate.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/common/model_builder.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/common/model_builder.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/common/model_builder_test.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/metal/benchmarking/main.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/metal_delegate.*@-g3 \
-    --per_file_copt=+tensorflow/lite/delegates/gpu/cl/gpu_api_delegate.*@-g3 \
-    --per_file_copt=+tensorflow/lite/experimental/examples/unity/TensorFlowLitePlugin/ProjectSettings/ProjectSettings.*@-g3 \
-    --per_file_copt=+tensorflow/lite/java/demo/gradle/wrapper/gradle-wrapper.*@-g3 \
-    --per_file_copt=+tensorflow/lite/java/ovic/demo/gradle/wrapper/gradle-wrapper.*@-g3 \
-    --per_file_copt=+tensorflow/core/grappler/optimizers/memory_optimizer.*@-g3 \
-    --per_file_copt=+tensorflow/tsl/framework/contraction/eigen_contraction_kernel.*@-g3 \
-    --per_file_copt=+tensorflow/tsl/framework/convolution/eigen_spatial_convolutions-inl.*@-g3 \
-    --per_file_copt=+tensorflow/tsl/framework/convolution/eigen_spatial_convolutions.*@-g3 \
-    --per_file_copt=+tensorflow/tsl/framework/fixedpoint/PacketMathAVX.*@-g3 \
-    --per_file_copt=+tensorflow/tsl/framework/fixedpoint/PacketMathAVX512.*@-g3 \
-    --per_file_copt=+tensorflow/tsl/framework/fixedpoint/PacketMathAVX2.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/sparse_matmul_op.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/sparse_matmul_op_test.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/depthwise_conv_op.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/deep_conv2d.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/eigen_cuboid_convolution.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/depthwise_conv_op.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/sparse_matmul_op.*@-g3 \
-    --per_file_copt=+tensorflow/core/kernels/depthwise_conv_grad_op.*@-g3 \
-    //tensorflow/tools/pip_package:build_pip_package || exit
+/usr/local/bin/bazel build --config=dbg //tensorflow/tools/pip_package:build_pip_package || exit
+
+#/usr/local/bin/bazel build --config=dbg \
+#    --per_file_copt=+tensorflow/compiler/xla/pjrt/transpose_kernels.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/gl_delegate.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/delegate.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/common/model_builder.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/common/model_builder.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/common/model_builder_test.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/metal/benchmarking/main.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/metal_delegate.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/delegates/gpu/cl/gpu_api_delegate.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/experimental/examples/unity/TensorFlowLitePlugin/ProjectSettings/ProjectSettings.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/java/demo/gradle/wrapper/gradle-wrapper.*@-g3 \
+#    --per_file_copt=+tensorflow/lite/java/ovic/demo/gradle/wrapper/gradle-wrapper.*@-g3 \
+#    --per_file_copt=+tensorflow/core/grappler/optimizers/memory_optimizer.*@-g3 \
+#    --per_file_copt=+tensorflow/tsl/framework/contraction/eigen_contraction_kernel.*@-g3 \
+#    --per_file_copt=+tensorflow/tsl/framework/convolution/eigen_spatial_convolutions-inl.*@-g3 \
+#    --per_file_copt=+tensorflow/tsl/framework/convolution/eigen_spatial_convolutions.*@-g3 \
+#    --per_file_copt=+tensorflow/tsl/framework/fixedpoint/PacketMathAVX.*@-g3 \
+#    --per_file_copt=+tensorflow/tsl/framework/fixedpoint/PacketMathAVX512.*@-g3 \
+#    --per_file_copt=+tensorflow/tsl/framework/fixedpoint/PacketMathAVX2.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/sparse_matmul_op.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/sparse_matmul_op_test.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/depthwise_conv_op.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/deep_conv2d.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/eigen_cuboid_convolution.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/depthwise_conv_op.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/sparse_matmul_op.*@-g3 \
+#    --per_file_copt=+tensorflow/core/kernels/depthwise_conv_grad_op.*@-g3 \
+#    //tensorflow/tools/pip_package:build_pip_package || exit
 
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg || exit
 
