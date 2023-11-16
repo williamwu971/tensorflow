e80538a2ce9f8984fd45a9ff7e0ee51406e836e0
William <william.wu.xiaoxiang@gmail.com>
2023-11-16 23:38:10 +1100

    -AUTO- Thu 16 Nov 2023 23:38:10 AEDT

---
+++ Tensor/down.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+scp xiaoxiang@labos.cs.usyd.edu.au:/mnt/sdb/xiaoxiang/.cache/bazel/_bazel_xiaoxiang/2b8bd3a91e41c898147a446c04118590/external/eigen_archive/unsupported/Eigen/CXX11/src/Tensor/$1 $1
